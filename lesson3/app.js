cb21f1956708c75a4ed38451e7e6c3cd5e5d276a
Junchen He <junchenhe123@gmail.com>
2024-05-29 12:09:41 -0700

    first commit

---
+++ lesson3/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+// express app
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for request
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    // const blogs = [
+    //     {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //     {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //     {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    // ];
+    res.render('index', {title: 'Home', blogs: blogs});
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {title: 'About'})
+})
+
+// create
+app.get('/blogs/create', (req, res)=>{
+    res.render('create', {title: 'Create'})
+})
+
+// 404 page
+app.use((req, res)=>{
+    res.status(404).render('404', {title: '404'})
+})
