a3e7c81c9ddf238cce2c42c121fb7c1575d9ceec
Junchen He <tim@Junchens-MacBook-Pro.local>
2024-05-31 17:45:20 -0700

    blog

--- lesson3/app.js
+++ lesson3/app.js
@@ -22,63 +22,56 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'))
+// encode the url body
+app.use(express.urlencoded({extended: true}));
 // log middleware
 app.use(morgan('dev'));
 
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res)=> {
-    const blog = new Blog({
-        title: 'new blog2',
-        snippet: 'about my new blog2',
-        body: 'more about my new blog2'
-    });
-
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-})
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err)=> {
-            console.log(err);
-        })
-})
-
-app.get('/single-blog', (req, res)=> {
-    Blog.findById('665a1f87f82c0989efcbb997')
-        .then((result)=> {
-            res.send(result);
-        })
-        .catch((err)=> {
-            console.log(err);
-        })
-})
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+  });
 
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    ];
-    res.render('index', {title: 'Home', blogs: blogs});
+    res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'})
 })
 
-// create
-app.get('/blogs/create', (req, res)=>{
-    res.render('create', {title: 'Create'})
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then(() => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
 })
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+})
+
+// blog route
+app.get('/blog/create', (req, res) => {
+    res.render('create', {title: 'Create'});
+})
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result)=> {
+            res.render('index', {title: 'All Blogs', blogs: result})
+        })
+        .catch((err)=> {
+            console.log(err);
+        });
+})
+
 
 // 404 page
 app.use((req, res)=>{
